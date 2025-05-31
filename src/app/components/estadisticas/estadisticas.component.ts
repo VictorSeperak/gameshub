fedfc1105924f3abb55cee6a34a4c58373050e28
VictorSeperak <victorseperakreyes03@gmail.com>
2025-05-30 22:09:01 -0500

    Parte 2 completo

--- src/app/components/estadisticas/estadisticas.component.ts
+++ src/app/components/estadisticas/estadisticas.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { JuegosDataService } from '../../services/juegos-data.service';
+import { Juego } from '../../interfaces/juego.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-estadisticas',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './estadisticas.component.html',
-  styleUrl: './estadisticas.component.css'
+  styleUrls: ['./estadisticas.component.css']
 })
-export class EstadisticasComponent {
+export class EstadisticasComponent implements OnInit {
+  totalJuegos = 0;
+  juegosGratis = 0;
+  juegosDePago = 0;
+  juegoConMejorRating?: Juego;
+  promedioPrecios = 0;
 
+  constructor(private juegosDataService: JuegosDataService) {}
+
+  ngOnInit(): void {
+    this.juegosDataService.obtenerJuegos().subscribe(juegos => {
+      this.totalJuegos = juegos.length;
+      this.juegosGratis = juegos.filter(j => j.esGratis).length;
+      this.juegosDePago = juegos.filter(j => !j.esGratis).length;
+      this.juegoConMejorRating = juegos.reduce((max, actual) =>
+        actual.rating > max.rating ? actual : max
+      );
+      const precios = juegos.filter(j => !j.esGratis).map(j => j.precio);
+      this.promedioPrecios = precios.length > 0
+        ? precios.reduce((acc, precio) => acc + precio, 0) / precios.length
+        : 0;
+    });
+  }
 }
